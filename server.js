b817cd673011c89f219a447d883536bb09410c5f
Kiera Walton <kwalton@qa.com>
2021-02-19 10:22:11 +0000

    added nyc - test coverage tool

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const createError = require(`http-errors`);
+// product url details here
 
 // instantiate my app 
 const app = express();
